aad2b980e46427e0c996b3e0ae6fcdb316a7a1cf
satishgsk87 <57361693+satishgsk87@users.noreply.github.com>
2020-02-01 01:08:09 +0530

    Update mod_ec2.tf

--- modules/ec2/mod_ec2.tf
+++ modules/ec2/mod_ec2.tf
@@ -4,16 +4,16 @@ resource "aws_instance" "myweb" {
    security_groups = ["default"]
    key_name = "${var.wista-project-key}"
    user_data = <<EOF
-       #! /bin/bash
-       sudo yum update -y 
-       sudo yum install git -y 
-       sudo cd /home/
-       sudo git clone https://github.com/satishgsk87/Terraform-CI-CD.git
-       sudo cd /home/Terraform-CI-CD
-       sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
-       sudo yum install puppet-agent -y
-       sudo export PATH=$PATH:/opt/puppetlabs/puppet/bin/
-       sudo puppet apply httpd.pp 
+   #! /bin/bash
+   sudo yum update -y 
+   sudo yum install git -y 
+   sudo cd /
+   sudo git clone -q https://github.com/satishgsk87/Terraform-CI-CD.git
+   sudo cd /Terraform-CI-CD/
+   sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+   sudo yum install puppet-agent -y
+   export PATH=$PATH:/opt/puppetlabs/puppet/bin/
+   sudo puppet apply httpd.pp 
    EOF
    
    tags = {
