871b61d734e77cf2678dd1353718898556fc995a
satishgsk87 <57361693+satishgsk87@users.noreply.github.com>
2020-02-01 00:24:58 +0530

    puppet cmds

---
+++ httpd.pp
@@ -0,0 +1,14 @@
+exec {'install httpd':
+  command => 'sudo yum install httpd -y',
+  path => /bin/
+}
+
+exec {'Change Text':
+  command => "sed -i 's/Test Page/Wista final project/g' /usr/share/httpd/noindex/index.html",
+  path => /bin/
+}
+
+exec {'start httpd':
+  command => 'sudo systemctl start httpd',
+  path => /bin/
+}
