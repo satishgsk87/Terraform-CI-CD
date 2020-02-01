51b4bee0b2d55dfd5dd94800d562dd8cb4684cde
satishgsk87 <57361693+satishgsk87@users.noreply.github.com>
2020-02-01 21:15:54 +0530

    Update httpd.pp

--- httpd.pp
+++ httpd.pp
@@ -1,14 +1,20 @@
 exec {'install httpd':
   command => 'sudo yum install httpd -y',
-  path => /bin/
+  path => /bin/,
+  provider => shell,
+  user => 'ec2-user'
 }
 
 exec {'Change Text':
   command => "sudo sed -i 's/Test Page/Wista final project/g' /usr/share/httpd/noindex/index.html",
-  path => /bin/
+  path => /bin/,
+  provider => shell,
+  user => 'ec2-user'
 }
 
 exec {'start httpd':
   command => 'sudo systemctl start httpd',
-  path => /bin/
+  path => /bin/,
+  provider => shell,
+  user => 'ec2-user'
 }
